8f7f02734947931a970977250239430b6ed1443d
evandro <ebolzan@inf.ufsm.br>
2021-09-02 18:07:10 -0300

    render e functions props

--- src/Props.js
+++ src/Props.js
@@ -3,19 +3,21 @@ import Panel from './Panel';
 
 class Props extends Component {
 
-     value='';
+     value= { name: "Evandro"};
+
+     outra = (valor) => { return  "*".repeat(valor.length)+valor};
 
     constructor(props){
         super(props);
         const {attr, attr2, attr3} = this.props;
         console.log(attr, attr2, attr3);
-        this.value = attr3
+       // this.value = attr3
     }
     render(){
         return (
             <div>
-            <h1>Hello world {this.value}</h1>
-            <Panel headertext="this a header" headercolor="red" data="this a data panel" />
+            <h1>Hello world {this.value.name}</h1>
+            <Panel headertext="this a header" headercolor="red" data={this.outra("ebolzan")} />
             </div>
         );
     }
